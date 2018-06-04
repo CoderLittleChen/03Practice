a953c0b5be7a4645ee46f726b8561f26b43be1de
清风徐来丶 <603568511@qq.com>
2018-06-04 15:06:44 +0800

    2018.06.04 15.06

--- 19Xml中的xpath查找某节点/Program.cs
+++ 19Xml中的xpath查找某节点/Program.cs
@@ -31,11 +31,11 @@ namespace _19Xml中的xpath查找某节点
             //          root//LinixServer/@name                       返回的是节点对应的属性对象  需要通过Value字段来获取值
             //          root//LinixServer[@name='CentOS']       返回的是节点
             //          root/Conputer|root/LinixServer               使用|可以获得多重模式的节点
-             
+
             //这里注意  拿到xml节点的属性对象  一定要通过value值来进行输出  
             XmlNode node = xdoc.SelectSingleNode("root//LinixServer/@name");
             //然后直接通过node.value 进行输出 
-            Console.WriteLine(node.Value); 
+            Console.WriteLine(node.Value);
             //Console.WriteLine(node.Attributes["price"].Value);
             Console.ReadKey();
 
